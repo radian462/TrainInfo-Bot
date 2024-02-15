e04afd61b8e7ac7826dd9859f5678111939f96dc
radian462 <102207047+radian462@users.noreply.github.com>
2024-02-15 04:38:47 +0000

    大幅改良

--- main.py
+++ main.py
@@ -1,71 +1,121 @@
 from atproto import Client
-import requests
-import re
 import time
 import datetime
+import requests
+import re
+import redis
 import os
+import json
+
+r = redis.Redis(
+  host='apn1-probable-gator-33492.upstash.io',
+  port=33492,
+  password= os.getenv("upstash_passward"),
+  ssl=True
+)
 
 client = Client()
-client.login("train-kanto.f5.si", os.getenv("passward"))
-
-train_data = requests.get("https://ntool.online/data/train_all.json").json()
-kanto = train_data['data']['4']
+client.login("train-kanto.f5.si", os.getenv("password"))
 
 second = datetime.datetime.now().time().second
 time.sleep(60 - second)
 
 old_message = ""
+
+def get_traindata():
+  site_source = requests.get("https://mainichi.jp/traffic/etc/a.html").text
+  site_source = re.sub("\n" , "#" , site_source)
+
+  site_data = re.search(r'関東エリア(.*?)<td colspan="3">', site_source).group(1)
+  site_data = re.sub("#" , "\n" , site_data)
+
+  train = re.findall(r'<td height="40"><font size="-1">(.*?)<BR><strong>', site_data)
+  status = re.findall(r'>(.*?)</font></strong></font></td>', site_data)
+  info = re.findall(r'<td height="40"><font size="-1">(.*?)</font></td>', site_data)
+
+  emojidict = {"列車遅延": "🕒列車遅延", "運転見合わせ": "🛑運転見合わせ", "運転情報": "ℹ️運転情報", "運転状況": "ℹ️運転状況", "運転再開":"🚋運転再開","平常運転":"🚋平常運転","その他":"⚠️その他"}
+
+  status = [emojidict.get(s, emojidict["その他"]) for s in status]
+  data = [{"train": t, "status": s, "info": i} for t, s, i in zip(train, status, info)]
+
+  return data
+
+def merge_data(olddata,newdata):
+  olddata_trains = [d["train"] for d in olddata]
+  newdata_trains = [d["train"] for d in newdata]
+
+  showdata = []
+  for i, train in enumerate(newdata_trains):
+    info = newdata[i]["info"]
+    if train in olddata_trains:
+      j = olddata_trains.index(train)
+      newstatus = newdata[i]["status"]
+      oldstatus = olddata[j]["status"]
+    else:
+      newstatus = newdata[i]["status"]
+      oldstatus = "🚋平常運転"
+
+    data = {"train":train,"oldstatus":oldstatus,"newstatus":newstatus,"info":info}
+    showdata.append(data)
+
+  for train in set(olddata_trains) - set(newdata_trains): 
+    i = olddata_trains.index(train)
+    newstatus = "🚋平常運転"
+    oldstatus = olddata[i]["status"]
+    info = "現在、ほぼ平常通り運転しています。"
+
+    data = {"train":train,"oldstatus":oldstatus,"newstatus":newstatus,"info":info}
+    showdata.append(data)
+
+  return showdata
+
+def make_message():
+  olddata = r.get('kanto_train')
+  olddata = json.loads(olddata)
+  newdata = get_traindata()
+
+  json_newdata = json.dumps(newdata)
+  r.set("kanto_train", json_newdata)
+
+  data = merge_data(olddata,newdata)
+  data_trains = [d["train"] for d in data]
+
+  message = ""
+  for train in data_trains:
+      t = data_trains.index(train)
+      if olddata == newdata:
+          message = "運行状況に変更はありません。"
+          uri = r.get('kanto_train_uri').decode('utf-8').strip('"') 
+          post_data = client.get_posts([uri])
+          try:
+            post_text = re.search(r"text='(.*?)'", str(post_data)).group(1)
+            if post_text == message:
+              client.delete_post(uri)
+          except:
+            pass
+      else:
+        if data == []:
+          message = "関東の電車は全て正常に動いています"
+        if data[t]["oldstatus"] == data[t]["newstatus"]:
+          if data[t]["oldstatus"] != "🚋平常運転":
+             message += f'{data[t]["train"]} : {data[t]["newstatus"]}\n{data[t]["info"]}\n\n'
+        elif data[t]["oldstatus"] != data[t]["newstatus"]:
+          message += f'{data[t]["train"]} : {data[t]["oldstatus"]}➡️{data[t]["newstatus"]}\n{data[t]["info"]}\n\n'
+
+  while message.endswith('\n'):
+    message= message[:-1]
+
+  return message
+
 while True:
-    railName = []
-    status = []
-    railCode = []
-    info = []
     current_time = time.localtime()
     minutes = current_time.tm_min
-    hour = current_time.tm_hour
-    hour = hour + 9
     print(minutes)
 
-    if minutes in [0, 15, 30, 45 ,52]:
-        for i in kanto:
-            if i["status"] != "平常運転":
-                railName.append(i["railName"])
-                status.append(i["status"])
-                railCode.append(i["railCode"])
-
-        emojidict = {"列車遅延": "🕒", "運転見合わせ": "🛑", "運転情報": "ℹ️", "運転状況": "ℹ️", "運転再開":"🚋","その他":"⚠️"}
-        status = [emojidict.get(data, "") + data for data in status]
-
-        for i in railCode:
-            info_sourse = requests.get(f"https://transit.yahoo.co.jp/diainfo/{i}/0").text
-            info_sourse = re.search(r'<dd class="trouble"><p>(.*?)<span>', info_sourse)
-            if info_sourse is not None:
-              info.append(info_sourse.group(1))
-              
-            elif '<dt><span class="icnNormalLarge"></span>平常運転</dt>' in info_sourse:
-                number = railCode.index(i)
-                del railName[number]
-                del status[number]
-                del railCode[number]
-              
-            else:
-              info.append("詳細の取得に失敗しました")
-
-        message = ""
-        if railName == []:
-            message = "関東の電車は全て正常に動いています"
-        else:
-          for i in range(len(railName)):
-              message += f"{railName[i]} : {status[i]}\n{info[i]}\n\n"
-
-        while message.endswith('\n'):
-          message= message[:-1]
-
-        if old_message != message:
-          client.send_post(text=message)
-        else:
-          client.send_post(text="電車の運行状況に変化はありません")
-
-        old_message = message
-
+    if minutes in [0,10,20,30,40,50,60,21,22,23]:
+      message = make_message()
+      print(message)
+      post = client.send_post(text=message)
+      r.set("kanto_train_uri", post.uri)
+  
     time.sleep(60)
