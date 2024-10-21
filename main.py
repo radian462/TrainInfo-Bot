6c6461ec753d8127f6c73a6af0971e4d6a2a47ec
radian 462 <kumita435675@gmail.com>
2024-10-21 11:33:22 +0000

    postを追加(恐らく動作するはず)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from atproto import Client
+import atproto
 from bs4 import BeautifulSoup
 import json
 import os
@@ -7,12 +7,13 @@ import requests
 from redis import Redis
 
 r = Redis(
-    host = os.getenv("UPSTASH_HOST"),
-    port = os.getenv("UPSTASH_PORT"),
-    password= os.getenv("UPSTASH_PASS"),
+    host=os.getenv("UPSTASH_HOST"),
+    port=os.getenv("UPSTASH_PORT"),
+    password=os.getenv("UPSTASH_PASS"),
     ssl=True,
-    decode_responses=True
+    decode_responses=True,
 )
+
 
 class TrainInfo:
     def __init__(self, region, bluesky_name, bluesky_pass, r):
@@ -21,43 +22,53 @@ class TrainInfo:
         self.bluesky_pass = bluesky_pass
 
         self.r = r
-        
-        '''
-        self.client = Client()
+
+        self.client = atproto.Client()
         self.client.login(self.bluesky_name, self.bluesky_pass)
 
         print(region + "リージョンログイン")
-        '''
 
     def request(self):
-        regions = {"関東":"4","関西":"6"}
+        regions = {"関東": "4", "関西": "6"}
         url = f"https://www.nhk.or.jp/n-data/traffic/train/traininfo_area_0{regions[self.region]}.json"
         response = requests.get(url)
 
         if response.status_code == 200:
-            original_data = response.json()['channel']['item'] + response.json()['channel']['itemLong']
+            original_data = (
+                response.json()["channel"]["item"]
+                + response.json()["channel"]["itemLong"]
+            )
 
             data = [
                 {
-                    'train': o['trainLine'],
-                    'status': re.sub(r'^.*? ', '', o['title']),
-                    'detail': o['textLong'],
+                    "train": o["trainLine"],
+                    "status": re.sub(r"^.*? ", "", o["title"]),
+                    "detail": o["textLong"],
                 }
                 for o in original_data
             ]
         else:
             url = "https://mainichi.jp/traffic/etc/a.html"
             response = requests.get(url)
-            region_html = re.search(f'{self.region}エリア(.*?)<td colspan="3">', re.sub('<strong>', '\n', response.text), re.DOTALL).group(1)
-            soup = BeautifulSoup(region_html, 'html.parser')
-            region_text = re.sub('\n\n', '', soup.get_text())
+            region_html = re.search(
+                f'{self.region}エリア(.*?)<td colspan="3">',
+                re.sub("<strong>", "\n", response.text),
+                re.DOTALL,
+            ).group(1)
+            soup = BeautifulSoup(region_html, "html.parser")
+            region_text = re.sub("\n\n", "", soup.get_text())
 
-            data_list = [t for t in region_text.split() if re.search(r'[ぁ-ゖァ-ヶ一-龍]', t)]
+            data_list = [
+                t for t in region_text.split() if re.search(r"[ぁ-ゖァ-ヶ一-龍]", t)
+            ]
             train = [data_list[i] for i in range(0, len(data_list), 3)]
-            status = [data_list[i+1] for i in range(0, len(data_list) - 1, 3)]
-            detail = [data_list[i+2] for i in range(0, len(data_list) - 2, 3)]
-            
-            data = [{"train": t, "status": s, "detail": d} for t, s, d in zip(train, status, detail)]
+            status = [data_list[i + 1] for i in range(0, len(data_list) - 1, 3)]
+            detail = [data_list[i + 2] for i in range(0, len(data_list) - 2, 3)]
+
+            data = [
+                {"train": t, "status": s, "detail": d}
+                for t, s, d in zip(train, status, detail)
+            ]
 
         status_emoji = {
             "平常運転": "🚋",
@@ -69,49 +80,96 @@ class TrainInfo:
             "運転見合わせ": "🛑",
             "その他": "⚠️",
         }
-        
+
         for d in data:
             for key in status_emoji.keys():
-                if key in d['status']:
-                    d['status'] = status_emoji[key] + key
+                if key in d["status"]:
+                    d["status"] = status_emoji[key] + key
 
         return data
 
-    def make_message(self,data):
-        db_region = {"関東":"kanto_train_test","関西":"kansai_train_test"}
-        
+    def make_message(self, data):
+        db_region = {"関東": "kanto_train_test", "関西": "kansai_train_test"}
+
         old = json.loads(self.r.get(db_region[self.region]))
         trains = set([d["train"] for d in data] + [d["train"] for d in old])
-        
+
         merged = [
             {
                 "train": t,
-                "oldstatus": (next((o['status'] for o in old if o['train'] == t), '🚋平常運転')),
-                "newstatus": (next((d['status'] for d in data if d['train'] == t), '🚋平常運転')),
-                "detail": (next((d['detail'] for d in data if d['train'] == t), '現在、ほぼ平常通り運転しています。'))
+                "oldstatus": (
+                    next((o["status"] for o in old if o["train"] == t), "🚋平常運転")
+                ),
+                "newstatus": (
+                    next((d["status"] for d in data if d["train"] == t), "🚋平常運転")
+                ),
+                "detail": (
+                    next(
+                        (d["detail"] for d in data if d["train"] == t),
+                        "現在、ほぼ平常通り運転しています。",
+                    )
+                ),
             }
             for t in trains
         ]
-    
-        #変更点があるものを前に
-        merged = [m for m in merged if m['oldstatus'] != m['newstatus']] + [m for m in merged if m['oldstatus'] == m['newstatus']]
-        
+
+        # 変更点があるものを前に
+        merged = [m for m in merged if m["oldstatus"] != m["newstatus"]] + [
+            m for m in merged if m["oldstatus"] == m["newstatus"]
+        ]
+
         messages = []
         for m in merged:
-            if m['newstatus'] == m['oldstatus']:
+            if m["newstatus"] == m["oldstatus"]:
                 messages.append(f"{m['train']} : {m['newstatus']}\n{m['detail']}")
             else:
-                messages.append(f"{m['train']} : {m['oldstatus']}➡️{m['newstatus']}\n{m['detail']}")
-         
-        #self.r.set(db_region[self.region],json.dumps(data))
+                messages.append(
+                    f"{m['train']} : {m['oldstatus']}➡️{m['newstatus']}\n{m['detail']}"
+                )
+
+        self.r.set(db_region[self.region], json.dumps(data))
         return messages
 
-kanto = TrainInfo(
-    "関東",
-    os.getenv("KANTO_NAME"),
-    os.getenv("KANTO_PASS"),
-    r
-)
+    def post(self, messages, service="Bluesky"):
+        messages_list = []
+        processing_message = ""
+        if not messages:
+            processing_message = self.region + "の電車は全て正常に運行しています。"
+        else:
+            for m in messages:
+                if len(processing_message + m + "\n\n") <= 300:
+                    processing_message += m + "\n\n"
+                else:
+                    messages_list.append(processing_message.rstrip("\n\n"))
+                    processing_message = ""
+
+        if service == "Bluesky":
+            for m in messages_list:
+                if messages_list.index(m) == 0:
+                    post = self.client.send_post(m)
+                    root_post_ref = atproto.models.create_strong_ref(post)
+                elif message_list.index(m) == 1:
+                    reply_to_root = atproto.models.create_strong_ref(
+                        self.client.send_post(
+                            text=m,
+                            reply_to=atproto.models.AppBskyFeedPost.ReplyRef(
+                                parent=root_post_ref, root=root_post_ref
+                            ),
+                        )
+                    )
+                else:
+                    reply_to_root = atproto.models.create_strong_ref(
+                        self.client.send_post(
+                            text=m,
+                            reply_to=atproto.models.AppBskyFeedPost.ReplyRef(
+                                parent=reply_to_root, root=root_post_ref
+                            ),
+                        )
+                    )
+
+
+kanto = TrainInfo("関東", os.getenv("KANTO_NAME"), os.getenv("KANTO_PASS"), r)
 
 data = kanto.request()
-print(kanto.make_message(data))
+messages = kanto.make_message(data)
+kanto.post(messages)
