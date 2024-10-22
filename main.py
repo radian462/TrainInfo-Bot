80757f332fb175799ee45891a114f5bab488914e
radian462 <102207047+radian462@users.noreply.github.com>
2024-10-23 08:39:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -177,7 +177,7 @@ class TrainInfo:
                     processing_message += m + "\n\n"
                 else:
                     messages_list.append(processing_message)
-                    processing_message = m + "\n\n"
+                    processing_message = m
             messages_list.append(processing_message)
 
         if service == "Bluesky":
