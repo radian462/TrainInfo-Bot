fe08af4962957676d8fc62dbb2bb3380dcff807d
radian462 <102207047+radian462@users.noreply.github.com>
2025-02-13 05:29:57 +0000

    スレッドにならなかったバグを修正

--- Modules/Clients/bluesky.py
+++ Modules/Clients/bluesky.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from typing import Optional
+from urllib.parse import urlparse
 
 import requests
 
@@ -50,6 +51,71 @@ class Bluesky:
             self.logger.error("An error occurred", exc_info=True)
             return None
 
+    def _refresh_token(self):
+        url = HOST + "com.atproto.server.refreshSession"
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.refreshjwt}"
+        }
+
+        response = requests.post(url, headers=headers)
+        session_data = response.json()
+
+        self.accessjwt = session_data.get("accessJwt")
+        self.refreshjwt = session_data.get("refreshJwt")
+
+    def _parse_uri(self, uri: str) -> dict:
+        try:
+            parsed_uri = urlparse(uri)
+            return {
+                "repo": parsed_uri.netloc,
+                "collection": parsed_uri.path.split("/")[1],
+                "rkey": parsed_uri.path.split("/")[2]
+            }
+        except Exception:
+            self.logger.error("An error occurred", exc_info=True)
+            return {}
+
+
+    def _get_reply_refs(self, uri: str) -> dict:
+        try:
+            url = HOST + "com.atproto.repo.getRecord"
+            uri_parts = self._parse_uri(uri)
+
+            r = requests.get(url, params=uri_parts)
+            parent = r.json()
+
+            parent_reply = parent["value"].get("reply")
+            if parent_reply is not None:
+                root_uri = parent_reply["root"]["uri"]
+                root_repo, root_collection, root_rkey = root_uri.split("/")[2:5]
+                r = requests.get(
+                    HOST + "com.atproto.repo.getRecord",
+                    params={
+                        "repo": root_repo,
+                        "collection": root_collection,
+                        "rkey": root_rkey,
+                    },
+                )
+                root = r.json()
+            else:
+                root = parent
+
+            return {
+                "root": {
+                    "uri": root["uri"],
+                    "cid": root["cid"],
+                },
+                "parent": {
+                    "uri": parent["uri"],
+                    "cid": parent["cid"],
+                },
+            }
+        except Exception:
+            self.logger.error("An error occurred", exc_info=True)
+            return {}
+
+
     def post(self, text: str, reply_to: Optional[dict] = None) -> dict:
         try:
             """
@@ -81,16 +147,7 @@ class Bluesky:
             }
 
             if reply_to:
-                data["record"]["reply"] = {
-                    "root": {
-                        "uri": reply_to.get("uri"),
-                        "cid": reply_to.get("cid"),
-                    },
-                    "parent": {
-                        "uri": reply_to.get("uri"),
-                        "cid": reply_to.get("cid"),
-                    },
-                }
+                data["record"]["reply"] = self._get_reply_refs(reply_to["uri"])
 
             response = requests.post(url, json=data, headers=headers)
 
