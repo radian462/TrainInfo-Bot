fc408a033aac48739a980a250a3861dff7e960c0
radian462 <102207047+radian462@users.noreply.github.com>
2025-11-12 01:51:03 +0000

    NHKとメイン処理を実装

--- traininfo/request.py
+++ traininfo/request.py
@@ -5,6 +5,8 @@ import requests
 from enums import Region
 from helpers.make_logger import make_logger
 
+from .normalizer import status_normalizer
+
 
 @dataclass(frozen=True)
 class TrainStatus:
@@ -14,12 +16,28 @@ class TrainStatus:
 
 
 class TrainInfoClient:
-    def __init__(self, region: Region, yahoo_app_id: str | None = None) -> None:
+    def __init__(
+        self,
+        region: Region,
+        proxy: dict[str, str] | None,
+        timeout: int = 10,
+        yahoo_app_id: str | None = None,
+    ) -> None:
         self.region = region
-        self.yahoo_app_id = yahoo_app_id
+        self.proxy = proxy
+        self.timeout = timeout
 
         self.logger = make_logger(type(self).__name__, context=region.label.upper())
         self.session = requests.Session()
+        self.session.proxies = proxy
+
+        self.yahoo_app_id = yahoo_app_id
+        self.NHK_ENDPOINT = (
+            "https://www.nhk.or.jp/n-data/traffic/train/traininfo_area_0%s.json"
+        )
+        self.YAHOO_ENDPOINT = (
+            "https://cache-diainfo-transit.yahooapis.jp/v4/diainfo/train"
+        )
 
         if not self.yahoo_app_id:
             self.logger.warning(
@@ -27,10 +45,42 @@ class TrainInfoClient:
             )
 
     def request(self) -> tuple[TrainStatus]:
-        pass
+        result = self._request_from_NHK()
+        if not result:
+            self.logger.warning("No data from NHK, trying Yahoo...")
+            result = self._request_from_yahoo()
 
-    def _request_from_NHK(self) -> tuple[TrainStatus]:
-        pass
+            if not result:
+                self.logger.error("No data received from all sources.")
+                return tuple()
 
-    def _request_from_yahoo(self) -> tuple[TrainStatus]:
+        return result
+
+    def _request_from_NHK(self, retry_times: int = 3) -> tuple[TrainStatus]:
+        for i in range(retry_times):
+            try:
+                response = self.session.get(
+                    self.NHK_ENDPOINT % self.region.value,
+                    timeout=self.timeout,
+                )
+
+                response.raise_for_status()
+                original_data = response.json().get("channel", {}).get(
+                    "item"
+                ) + response.json().get("channel", {}).get("itemLong")
+                return tuple(
+                    TrainStatus(
+                        train=o.get("trainLine", ""),
+                        status=status_normalizer(o.get("status", "")),
+                        detail=o.get("textLong", ""),
+                    )
+                    for o in original_data
+                )
+            except Exception as e:
+                self.logger.error(f"Error requesting from NHK: {e}")
+                if i < retry_times - 1:
+                    self.logger.info(f"Retrying... ({i + 1}/{retry_times})")
+                    return tuple()
+
+    def _request_from_yahoo(self, retry_times: int = 3) -> tuple[TrainStatus]:
         pass
