0f9eb1da94d4cfa545f0c8d1174ca903e7bdaffb
radian462 <102207047+radian462@users.noreply.github.com>
2025-12-04 13:51:31 +0000

    docs: normalizer.pyにdocstringを追加

--- traininfo/normalizer.py
+++ traininfo/normalizer.py
@@ -14,6 +14,21 @@ STATUS_EMOJI: Final[dict] = {
 
 
 def status_normalizer(status: str) -> str:
+    """
+    運行状況を正規化する。
+    先頭に絵文字を付与して返す。運行状況が部分一致する場合も対応する。
+    対応する運行状況がない場合は「⚠️その他」を返す。
+
+    Parameters
+    ----------
+    status : str
+        正規化する運行状況。
+
+    Returns
+    -------
+    str
+        正規化された運行状況。
+    """
     for key in STATUS_EMOJI.keys():
         if key in status:
             return STATUS_EMOJI[key] + key
